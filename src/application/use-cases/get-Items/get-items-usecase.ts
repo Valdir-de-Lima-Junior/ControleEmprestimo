38edcb68aea7a8eac70bdc7e2b9e0c712380745a
Valdir de Lima Jr. <Limavaldirjunior@gmail.com>
2024-09-19 19:29:20 -0300

    pequeñaCorreção

--- src/application/use-cases/get-Items/get-items-usecase.ts
+++ src/application/use-cases/get-Items/get-items-usecase.ts
@@ -1,6 +1,6 @@
 import { ItemRepository } from "../../../domain/repository/item-repository";
 import { GetItemsInput } from "./get-items-input";
-import { GetItemsOutput } from "./get-tems-output";
+import { GetItemsOutput } from "./get-items-output";
 
 export class GetItemsUseCase{
     constructor(readonly itemRepository: ItemRepository){}
