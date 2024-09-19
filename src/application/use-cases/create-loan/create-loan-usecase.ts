304a83c4a9394ebb688269bde3fe93cea4dfbcc8
Marlon202004 <antunesdelimamarlon01@gmail.com>
2024-09-19 19:44:07 -0300

    correçaoloan

--- src/application/use-cases/create-loan/create-loan-usecase.ts
+++ src/application/use-cases/create-loan/create-loan-usecase.ts
@@ -2,7 +2,7 @@ import { LoanRepository } from "../../../domain/repository/loan-repository";
 import { CreateLoanOutput } from "./create-loan-output";
 import { CreateLoanInput } from "./create-loan-input";
 
-export class GetLoanUsecase{
+export class CreateLoanUsecase{
    constructor(readonly loanRepository: LoanRepository){}
 
    execute(input: CreateLoanInput): CreateLoanOutput {
