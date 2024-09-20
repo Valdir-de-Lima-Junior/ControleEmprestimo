71c061e47c7f0cbeb627310ac6684794b3d1aa3c
Valdir de Lima Junior <limavaldirjunior@gmail.com>
2024-09-19 22:20:37 -0300

    Output com Erro

--- src/application/use-cases/get-items/get-items-output.ts
+++ src/application/use-cases/get-items/get-items-output.ts
@@ -1,3 +1,10 @@
+type ItemTypeOutput = {
+    id: string;
+    name: string;
+}
+
 export type GetItemsOutput = {
-    
+    id: string;
+    name: string;
+    itemType: ItemTypeOutput;
 }
--- src/application/use-cases/get-items/get-items-usecase.ts
+++ src/application/use-cases/get-items/get-items-usecase.ts
@@ -5,9 +5,24 @@ import { GetItemsOutput } from "./get-items-output";
 export class GetItemsUseCase{
     constructor(readonly itemRepository: ItemRepository){}
 
-    execute(input: GetItemsInput): GetItemsOutput {
-        return {
-            name: ''
-        };
+    execute(input: GetItemsInput): GetItemsOutput[] {
+        const itemList = this.itemRepository.getAll();
+
+        const output: GetItemsOutput[] = [];
+
+        for (const item of itemList){
+            output.push{
+                {
+                    id: item.getId(),
+                    name: item.getName(),
+                    itemType: {
+                        id: item.getItemType().getId(),
+                        name: item.getItemType().getName()
+                    }
+                }
+            }
+        }
+
+        return output;
     }
 }
