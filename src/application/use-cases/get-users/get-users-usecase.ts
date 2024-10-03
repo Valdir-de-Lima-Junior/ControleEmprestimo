e4735dbfedfd12564ec8f36783081d656b8725c7
lucas <usecrispin@gmail.com>
2024-10-02 21:37:14 -0300

    commit

--- src/application/use-cases/get-users/get-users-usecase.ts
+++ src/application/use-cases/get-users/get-users-usecase.ts
@@ -5,7 +5,23 @@ import { GetUsersOutput } from "./get-users-output";
 export class GetUsersUseCase{
     constructor(readonly userRepository: UserRepository){}
 
-    execute(input: GetUsersInput): GetUsersOutput{
-        return {};
+    execute(input: GetUsersInput): GetUsersInput{
+        const userList = this.userRepository.getAll();
+
+        const output: GetUsersOutput[] = [];
+
+        for(const user of userList){
+            output.push(
+                {
+                    id: user.getId(),
+                    name: user.getName(),
+                    person: {
+                        id: user.getPerson().getId(),
+                        name: user.getPerson().getName()
+                    }
+                }
+            )
+        }
+        return output;
     }
 }
