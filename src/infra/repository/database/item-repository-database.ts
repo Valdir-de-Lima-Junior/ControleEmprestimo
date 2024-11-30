61c4c2973a3a9f6049da17888f40d77e3fcaccf2
Marlon202004 <antunesdelimamarlon01@gmail.com>
2024-11-30 15:18:46 -0300

    item database

--- src/infra/repository/database/item-repository-database.ts
+++ src/infra/repository/database/item-repository-database.ts
@@ -6,21 +6,34 @@ export default class ItemRepositoryDatabase implements ItemRepository {
     
     constructor(private connection: Connection) {}
     
-    getAll(): Promise<Item[]> {
-        throw new Error("Method not implemented.");
-    }
-    getById(id: string): Promise<Item> {
-        throw new Error("Method not implemented.");
-    }
-    create(item: Item): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    update(item: Item): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    delete(id: string): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-
+    async getAll(): Promise<Item[]> {
+        const output = [];
+        const itemData = await this.connection.execute(´
+        SELECT item.nome AS nome, item.preco AS preco, item.quantidade AS quantidade, item.imagem AS imagem,
+        item.descricao AS descricao, item.id AS id, item.categoria_id AS categoria_id
+        FROM item
+        LEFT JOIN categoria ON item_categoria.categoria_id = categoria.id
+        ORDER BY item.id);
     
-}
+        for (const item of itemData) {
+        const itemEntity = new Item(
+            item.nome,
+            item.preco,
+            item.quantidade,
+            item.imagem,
+            item.descricao,
+            item.id,
+            item.categoria_id
+            );
+    
+            const item = new Item(
+                itemEntity.nome,
+                itemEntity.preco,
+                itemEntity.quantidade,
+                itemEntity.imagem,
+                itemEntity.descricao,
+                itemEntity.id,
+                itemEntity.categoria_id
+                );
+               
+    
--- src/infra/repository/database/itemType-repository-database.ts
+++ src/infra/repository/database/itemType-repository-database.ts
@@ -5,20 +5,23 @@ import { Connection } from "../../database/connection";
 export default class ItemTypeRepositoryDatabase implements ItemTypeRepository{
     constructor(private connection: Connection){}
  
-    getAll(): Promise<ItemType[]> {
-        throw new Error("Method not implemented.");
-    }
-    getById(id: string): Promise<ItemType> {
-        throw new Error("Method not implemented.");
-    }
-    create(item: ItemType): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    update(item: ItemType): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    delete(id: string): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
+    async  getAll(): Promise<ItemType[]> {
+        const output = [];
+        const ItemType = await this.connection.getRepository(ItemType).execute(´
+        SELECT ItemType.nome AS nome, ItemType.id AS id 
+        FROM ItemType
+        LEFT JOIN Item ON Item.idItemType = ItemType.id
+        ´);
     
-}
+        for(const item of ItemType){
+        const itemOutput = new ItemType(
+            item.id,
+            item.nome
+            );
+    
+        const itemOutput2 = new Item(
+            item.id,
+            item.nome,
+            item.idItemType
+            );
+    
