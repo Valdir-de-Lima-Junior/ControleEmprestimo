8f24848cbb83393390ce366d29d8df68ef540d1e
Valdir-de-Lima-Junior <limavaldirjunior@gmail.com>
2024-12-03 10:32:35 -0300

    Efetuar termino

--- src/infra/repository/database/item-repository-database.ts
+++ src/infra/repository/database/item-repository-database.ts
@@ -1,10 +1,33 @@
 import { Item } from "../../../domain/entity/item";
+import { ItemType } from "../../../domain/entity/item-type";
 import { ItemRepository } from "../../../domain/repository/item-repository";
 import { Connection } from "../../database/connection";
 
 export default class ItemRepositoryDatabase implements ItemRepository {
     
     constructor(private connection: Connection) {}
+    async getAll(): Promise<Item[]> {
+        const output = []
+        const itemsData = await this.connection.execute(`
+        select items.id as id, items.name as nome_item, id_items_type as id_tipo_items from items
+        left join items_type on items.id_items_type = items_type.id
+        `);
+
+        for (const itemData of itemsData) {
+            const itemType = new ItemType(
+                itemData.id_tipo_items
+            )
+
+            const item = new Item(
+                itemData.nome_item,
+                itemType,
+                itemData.id
+            )
+
+            output.push(item)
+        }
+        return output;
+    }
     getById(id: string): Promise<Item> {
         throw new Error("Method not implemented.");
     }
@@ -18,36 +41,4 @@ export default class ItemRepositoryDatabase implements ItemRepository {
         throw new Error("Method not implemented.");
     }
     
-    async getAll(): Promise<Item[]> {
-        const output = [];
-        const itemData = await this.connection.execute(`
-        SELECT item.nome AS nome, Item.preco AS preco, Item.quantidade AS quantidade, Item.imagem AS imagem,
-        Item.descricao AS descricao, Item.id AS id, Item.categoria_id AS categoria_id
-        FROM item
-        LEFT JOIN categoria ON item_categoria.categoria_id = categoria.id
-        ORDER BY Item.id `);
-        
-    
-        for (const item of itemData) {
-        const itemEntity = new Item(
-            item.nome,
-            item.preco,
-            item.quantidade,
-            item.imagem,
-            item.descricao,
-            item.id,
-            item.categoria_id
-            );
-           
-            const item = new Item(
-                itemEntity.nome,
-                itemEntity.preco,
-                itemEntity.quantidade,
-                itemEntity.imagem,
-                itemEntity.descricao,
-                itemEntity.id,
-                itemEntity.categoria_id
-                );
-               
-            }
-        }
+     
